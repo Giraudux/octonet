e79283757bd5c89c965fdbc871e52ca0a0920079
Giraudux <giraudux@gmail.com>
2014-12-08 15:14:58 +0000

    add virtual destructor

--- include/octonet/abstract_server.hpp
+++ include/octonet/abstract_server.hpp
@@ -12,6 +12,7 @@ class abstract_server
 
 	public:
 		abstract_server(octonet_notifier* _notifier) : notifier_(_notifier) {}
+		virtual abstract_server(void) {}
 		virtual void start(void) = 0;
 		virtual void stop(void) = 0;
 		virtual const std::string& server_id(void) const = 0;
