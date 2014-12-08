160eb98e5ea6949079a884b6ea89d1355c95aee4
Giraudux <giraudux@gmail.com>
2014-12-08 15:04:32 +0000

    add abstract_server

---
+++ include/octonet/abstract_server.hpp
@@ -0,0 +1,18 @@
+//          Copyright Alexis Giraudet 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+class octonet_notifier;
+
+class abstract_server
+{
+	protected:
+		octonet_notifier* notifier_;
+
+	public:
+		abstract_server(octonet_notifier* _notifier) : notifier_(_notifier) {}
+		virtual void start(void) = 0;
+		virtual void stop(void) = 0;
+		virtual const std::string& server_id(void) const = 0;
+};
