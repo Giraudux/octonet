da8d07366ccb53cd1969da952c8bba1f4f26ee8e
Giraudux <giraudux@gmail.com>
2014-12-09 14:52:05 +0000

    new arch

--- include/octonet/octonet.hpp
+++ include/octonet/octonet.hpp
@@ -11,61 +11,126 @@
 #include <boost/thread/thread.hpp>
 #include <set>
 
-#include "octonet_notifier.hpp"
 #include "octopeer.hpp"
 #include "octopeer_observer.hpp"
 #include "octoquery.hpp"
 #include "octoquery_observer.hpp"
 
-class octonet : public octonet_notifier
+class octonet : public octopeer
 {
 private:
+    boost::mutex peers_set_mtx_;
+    boost::mutex query_observers_set_mtx_;
+    boost::mutex peer_observers_set_mtx_;
 
-    void add_peer(const octopeer& _peer) {}
+    std::set<octopeer> peers_set_;
+    std::set<octoquery_observer*> query_observers_set_;
+    std::set<octopeer_observer*> peer_observers_set_;
 
-    void rem_peer(const octopeer& _peer) {}
+    void notify_peer_observers(const octopeer& _peer, octopeer_state _state)
+    {
+        boost::lock_guard<boost::mutex> guard(peer_observers_set_mtx_);
+        for(std::set<octopeer_observer*>::const_iterator it=peer_observers_set_.begin(); it!=peer_observers_set_.end(); ++it)
+        {
+            (*it)->update_peer(_peer, _state);
+        }
+    }
 
-    void receive_query(const octopeer& _peer, const octoquery& _query) {}
+    void notify_query_observers(const octopeer& _peer, const octoquery& _query)
+    {
+        boost::lock_guard<boost::mutex> guard(query_observers_set_mtx_);
+        for(std::set<octoquery_observer*>::const_iterator it=query_observers_set_.begin(); it!=query_observers_set_.end(); ++it)
+        {
+            if((*it)->app_id().empty() || ((*it)->app_id() == _query.app_id))
+            {
+                (*it)->update_query(_peer, _query);
+            }
+        }
+    }
+
+protected:
+
+    octonet(void) : octopeer(boost::uuids::random_generator()()) {}
+
+    virtual ~octonet(void) {}
+
+    bool add_peer(const octopeer& _peer)
+    {
+        boost::lock_guard<boost::mutex> guard(peers_set_mtx_);
+        if(peers_set_.find(_peer) == peers_set_.end())
+        {
+            if(peers_set_.insert(_peer).second);
+            {
+                notify_peer_observers(_peer, online);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool rem_peer(const octopeer& _peer)
+    {
+        boost::lock_guard<boost::mutex> guard(peers_set_mtx_);
+        if(peers_set_.erase(_peer) > 0)
+        {
+            notify_peer_observers(_peer, offline);
+            return true;
+        }
+        return false;
+    }
+
+
+    void receive_query(const octopeer& _peer, const octoquery& _query)
+    {
+        notify_query_observers(_peer, _query);
+    }
 
 public:
 
-    octonet(void) : octonet_notifier(boost::uuids::random_generator()()) {}
-
-    ~octonet(void) {}
-
     bool add_query_observer(octoquery_observer* _query_observer)
     {
-        boost::lock_guard<boost::mutex> guard(this->query_observers_set_mtx_);
-        return this->query_observers_set_.insert(_query_observer).second;
+        boost::lock_guard<boost::mutex> guard(query_observers_set_mtx_);
+        return query_observers_set_.insert(_query_observer).second;
     }
 
     bool rem_query_observer(octoquery_observer* _query_observer)
     {
-        boost::lock_guard<boost::mutex> guard(this->query_observers_set_mtx_);
-        return this->query_observers_set_.erase(_query_observer) > 0;
+        boost::lock_guard<boost::mutex> guard(query_observers_set_mtx_);
+        return query_observers_set_.erase(_query_observer) > 0;
     }
 
     bool add_peer_observer(octopeer_observer* _peer_observer)
     {
-        boost::lock_guard<boost::mutex> guard(this->peer_observers_set_mtx_);
-        return this->peer_observers_set_.insert(_peer_observer).second;
+        boost::lock_guard<boost::mutex> guard(peer_observers_set_mtx_);
+        return peer_observers_set_.insert(_peer_observer).second;
     }
 
     bool rem_peer_observer(octopeer_observer* _peer_observer)
     {
-        boost::lock_guard<boost::mutex> guard(this->peer_observers_set_mtx_);
-        return this->peer_observers_set_.erase(_peer_observer) > 0;
+        boost::lock_guard<boost::mutex> guard(peer_observers_set_mtx_);
+        return peer_observers_set_.erase(_peer_observer) > 0;
     }
 
-    void start(void)
+    std::set<octopeer>& peers(std::set<octopeer>& _peers)
     {
-        ;
+        boost::lock_guard<boost::mutex> guard(peers_set_mtx_);
+        _peers.insert(peers_set_.begin(), peers_set_.end());
+        return _peers;
     }
 
-    void stop(void)
+    std::set<std::string>& app_ids(std::set<std::string>& _app_ids)
     {
-        ;
+        boost::lock_guard<boost::mutex> guard(query_observers_set_mtx_);
+        for(std::set<octoquery_observer*>::const_iterator it=query_observers_set_.begin(); it!=query_observers_set_.end(); ++it)
+        {
+            _app_ids.insert((*it)->app_id());
+        }
+        return _app_ids;
     }
+
+    virtual void start(void) = 0;
+
+    virtual void stop(void) = 0;
 };
 
 #endif
